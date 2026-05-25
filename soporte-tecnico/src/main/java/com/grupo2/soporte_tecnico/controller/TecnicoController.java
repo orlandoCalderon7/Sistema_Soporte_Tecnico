17c03c2172de4bbf9fb60d5a5258f63b691d6fcb
orlandoCalderon7 <orlandocalderon2025@gmail.com>
2026-05-24 23:44:23 -0500

    Add NonNull annotation to PathVariable parameters

--- soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/controller/TecnicoController.java
+++ soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/controller/TecnicoController.java
@@ -5,6 +5,7 @@ import com.grupo2.soporte_tecnico.service.TecnicoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.lang.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,7 +36,7 @@ public class TecnicoController {
 
     @GetMapping("/{id}")
     @Operation(summary = "Obtener técnico por ID")
-    public ResponseEntity<Tecnico> obtenerPorId(@PathVariable Long id) {
+    public ResponseEntity<Tecnico> obtenerPorId(@PathVariable @NonNull Long id) {
         return ResponseEntity.ok(tecnicoService.obtenerPorId(id));
     }
 
@@ -47,14 +48,14 @@ public class TecnicoController {
 
     @PutMapping("/{id}")
     @Operation(summary = "Actualizar técnico")
-    public ResponseEntity<Tecnico> actualizar(@PathVariable Long id,
+    public ResponseEntity<Tecnico> actualizar(@PathVariable @NonNull Long id,
                                                @Valid @RequestBody Tecnico tecnico) {
         return ResponseEntity.ok(tecnicoService.actualizar(id, tecnico));
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Eliminar técnico")
-    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminar(@PathVariable @NonNull Long id) {
         tecnicoService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
