57807b6f3730993a3d93def4b062ba290784dcdf
orlandoCalderon7 <orlandocalderon2025@gmail.com>
2026-05-24 23:52:52 -0500

    Update SoporteTecnicoApplication.java

--- soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/SoporteTecnicoApplication.java
+++ soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/SoporteTecnicoApplication.java
@@ -1,13 +1,18 @@
 package com.grupo2.soporte_tecnico;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+    title = "Sistema Soporte Técnico",
+    version = "1.0.0",
+    description = "API REST para gestión de solicitudes de soporte técnico"
+))
 @SpringBootApplication
 public class SoporteTecnicoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SoporteTecnicoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SoporteTecnicoApplication.class, args);
+    }
 }
