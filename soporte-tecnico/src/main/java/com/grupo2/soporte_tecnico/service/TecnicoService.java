ae8645e6f2de75fdc84763331333fef95a4621d8
orlandoCalderon7 <orlandocalderon2025@gmail.com>
2026-05-24 23:50:21 -0500

    Add @NonNull annotations to TecnicoService methods

--- soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/service/TecnicoService.java
+++ soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/service/TecnicoService.java
@@ -1,13 +1,14 @@
 package com.grupo2.soporte_tecnico.service;
 
 import com.grupo2.soporte_tecnico.model.Tecnico;
+import org.springframework.lang.NonNull;
 import java.util.List;
 
 public interface TecnicoService {
     List<Tecnico> listarTodos();
     List<Tecnico> listarDisponibles();
-    Tecnico obtenerPorId(Long id);
+    Tecnico obtenerPorId(@NonNull Long id);
     Tecnico crear(Tecnico tecnico);
-    Tecnico actualizar(Long id, Tecnico tecnico);
-    void eliminar(Long id);
+    Tecnico actualizar(@NonNull Long id, Tecnico tecnico);
+    void eliminar(@NonNull Long id);
 }
