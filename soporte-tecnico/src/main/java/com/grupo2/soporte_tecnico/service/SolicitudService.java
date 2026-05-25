c9b9f23e9b3e639792fdb838977d8c1798758b20
orlandoCalderon7 <orlandocalderon2025@gmail.com>
2026-05-24 23:49:58 -0500

    Add null safety annotations to SolicitudService methods

--- soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/service/SolicitudService.java
+++ soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/service/SolicitudService.java
@@ -1,14 +1,16 @@
 package com.grupo2.soporte_tecnico.service;
 
+import com.grupo2.soporte_tecnico.model.EstadoSolicitud;
 import com.grupo2.soporte_tecnico.model.Solicitud;
+import org.springframework.lang.NonNull;
 import java.util.List;
 
 public interface SolicitudService {
     List<Solicitud> listarTodas();
-    Solicitud obtenerPorId(Long id);
-    List<Solicitud> listarPorCliente(Long clienteId);
-    List<Solicitud> listarPorEstado(String estado);
+    Solicitud obtenerPorId(@NonNull Long id);
+    List<Solicitud> listarPorCliente(@NonNull Long clienteId);
+    List<Solicitud> listarPorEstado(EstadoSolicitud estado);
     Solicitud crear(Solicitud solicitud);
-    Solicitud actualizar(Long id, Solicitud solicitud);
-    void eliminar(Long id);
+    Solicitud actualizar(@NonNull Long id, Solicitud solicitud);
+    void eliminar(@NonNull Long id);
 }
