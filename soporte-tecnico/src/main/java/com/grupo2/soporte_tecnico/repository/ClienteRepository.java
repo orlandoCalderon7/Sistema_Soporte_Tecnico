44166c5dce130c988f8536da3c9389520a12eb46
orlandoCalderon7 <orlandocalderon2025@gmail.com>
2026-05-24 23:47:53 -0500

    Remove redundant comment in ClienteRepository

--- soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/repository/ClienteRepository.java
+++ soporte-tecnico/src/main/java/com/grupo2/soporte_tecnico/repository/ClienteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     // findAll, findById, existsById, deleteById y save
-    // ya vienen incluidos en JpaRepository ✅
+    // ya vienen incluidos en JpaRepository 
 }
